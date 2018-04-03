c0996d526d483681e66bb975cc9b1dca8f7c2b70
xXAlphaManXx <smartclashcoc@gmail.com>
2018-04-03 23:13:09 +0530

    Working on dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
                     Manage everything here, in a single view.
                 </h5>
                 <div style="padding-top: 20px">
-                    <a href="" class="waves-effect waves-dark btn grey lighten-3 indigo-text">Profile</a>
+                    <a href="" class="waves-effect waves-dark btn white indigo-text">Profile</a>
                 </div>
             </div>
         </div>
@@ -24,8 +24,39 @@
 
     <section>
         <div class="container">
-            <h4 class="center-align">Open Hackeria</h4>
-            <hr width="25%" style="border-width: 3px">
+            <div class="row">
+                <div class="col s12 m8 l8">
+                    <div class="card">
+                        <div class="card-content">
+                            <span class="card-title">Something here</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4 l4">
+                    <div class="card">
+                        <div class="card-content">
+                            <img src="https://lorempixel.com/g/700/700/" class="circle responsive-img">
+                            <div class="center-align">
+                                <br />
+                                <span class="card-title">{{ auth()->user()->name }}</span>
+                                <div class="chip">
+                                    <i class="close material-icons">local_post_office</i> {{ auth()->user()->email }}
+                                </div>
+                                <div class="chip">
+                                    <i class="close material-icons">person</i>
+                                    @if(auth()->user()->type === 0)
+                                        Normal User
+                                    @else
+                                        Admin
+                                    @endif
+                                </div>
+                            </div>
+                            <h5>About Me</h5>
+                            <p>{{ auth()->user()->profile->bio }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
