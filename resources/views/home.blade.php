93bb1c0e831cede7db23e46c526ccd9280df7423
xXAlphaManXx <smartclashcoc@gmail.com>
2018-04-04 10:16:54 +0530

    Workin gon dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,16 +16,26 @@
                     Manage everything here, in a single view.
                 </h5>
                 <div style="padding-top: 20px">
-                    <a href="" class="waves-effect waves-dark btn grey lighten-3 indigo-text">Profile</a>
+                    <a href="" class="waves-effect waves-dark btn white indigo-text">Profile</a>
                 </div>
             </div>
         </div>
     </div>
 
     <section>
-        <div class="container">
-            <h4 class="center-align">Open Hackeria</h4>
-            <hr width="25%" style="border-width: 3px">
+        <div class="col s12 m8 offset-m2 l6 offset-l3">
+            <div class="card-panel grey lighten-5 z-depth-1">
+                <div class="row valign-wrapper">
+                    <div class="col s2">
+                        <img src="images/yuna.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                    </div>
+                    <div class="col s10">
+                        <span class="black-text">
+                            This is a square image. Add the "circle" class to it to make it appear circular.
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
